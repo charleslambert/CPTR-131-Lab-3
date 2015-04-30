216725f8fecd89fb544919b603f42040b17f0c29
Charles Lambert <c.lambert1213@gmail.com>
2015-04-30 15:02:21 -0700

    Version 3

--- server.rb
+++ server.rb
@@ -1,7 +1,20 @@
 require 'socket'
 
 class Server
-	def initilaze
+
+
+	def initialize()
 		@server = TCPServer.new 2000
+		client_hash = Hash.new
+
+
+		sever_run(server, client_hash)
 	end
 
+	def server_run(server)
+
+
+	end
+
+
+end
