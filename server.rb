3d8786749c1d80991c75ffec5c029d10932312c9
Charles Lambert <c.lambert1213@gmail.com>
2015-05-01 11:19:41 -0700

    Version 4-Code working for server

--- server.rb
+++ server.rb
@@ -5,36 +5,49 @@ class Server
 
 	def initialize()
 		@server = TCPServer.new 2000
-		client_hash = Hash.new
+		@client_hash = Hash.new
 	end
 
 	def server_run()
 		loop do
   			Thread.start(@server.accept) do |client|
   				c_message = client.gets
-  				s_message = command(c_message.strip, client, client_hash)
-  				client.puts s_message
-  				client.close
+
+  				if (/CONNECT\s+(\S+)/ =~ c_message) && (connect($1, client, @client_hash)) 					
+  					while(c_message != "DISCONNECT")
+  						c_message = client.gets
+  						s_message = command(c_message.strip, $1, client, @client_hash)
+  						client.puts s_message
+  					end
+  				else 
+  					client.close
+  				end
   			end
 		end
 
 	end
 
-	def command(recieved_m, client, client_hash)
+	def command(recieved_m, username, client, client_hash)
 		return_m = ""
 
 		case recieved_m
 
-		when /CONNECT\s+(\S+)/
-			return_m = connect($1, client_hash)
-		when /BROADCAST\s+(\S+)/
+		when /BROADCAST\s+(.++)/
+			broadcast($1, client_hash)
 			return_m = "SENT"
-		when /SEND\s+(\S+)\s+(\S)/
+		when /SEND\s+(\S+)\s+(.+)/
+			#.has_key?()
+			client_hash[$1].puts "SENTFROM #{username} #{$2}"
 			return_m = "SENT"
 		when /USERLIST/
+			client.puts "USERS #{client_hash.keys.inspect}"
 			return_m = "SENT"
 		when /DISCONNECT/
 			return_m = "DISCONNECTED"
+			client.puts return_m
+			client_hash.delete(username)
+			client.close
+
 		else
 			return_m = "Not a valid command"
 		end
@@ -44,11 +57,19 @@ class Server
 
 
 	def connect(username, client, client_hash)
-		if /\s/ =~ username
-			client_hash{username => client}
-			return "CONNECTED"
+		if ((/\s/ =~ username) == nil)
+			client_hash[username] = client
+			client.puts "CONNECTED"
+			return true
 		else
-			return "FAILED"
+			client.puts "FAILED"
+			return false
+		end
+	end
+
+	def broadcast(message, client_hash)
+		client_hash.each do |key, value|
+			value.puts message
 		end
 	end
 end
