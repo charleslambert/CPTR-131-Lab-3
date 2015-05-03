5451489becac8ca0e6ebac74c94a5e2fe6da1f1f
Charles Lambert <c.lambert1213@gmail.com>
2015-05-03 16:59:00 -0700

    Version 6-Server and client are working still having connection problems

--- client.rb
+++ client.rb
@@ -7,17 +7,45 @@ class Client
 	end
 
 	def client_run()
+
+		connect(@server)
+		
+		thr = nil
+		
 		loop do
-			user_input = gets.chomp
-			@server.puts user_input
-			s_message = @server.gets
-			puts s_message
+			if ((s_message = @server.gets).strip != nil)
+				puts s_message.strip
+				@server.puts "RECIEVED"
+			end
+
+			if (/DISCONNECTED*/ =~ s_message)
+				@server.close
+				exit
+			end
+
+			if(thr == nil)
+				thr = Thread.new {
+					user_input = gets.chomp
+					@server.puts user_input
+					thr = nil
+				}
+			end
 		end
 	end
 
-	def command(s_message)
+	def connect(server)
+		connected = false
+		while (connected != true)
+			attempt = gets.chomp
+			server.puts attempt
+			if /CONNECTED*/ =~ server.gets
+				connected = true
+			else
+				connected = false
+			end
+			server.puts "RECIEVED"
+		end
 	end
-
 end
 
 Client.new.client_run
--- server.rb
+++ server.rb
@@ -11,17 +11,17 @@ class Server
 		loop do
   			Thread.start(@server.accept) do |client|
 
-  				connect(client, @client_hash)
+  				username = connect(client, @client_hash)
 
   				c_message = ""
 
-  				while(c_message != "DISCONNECTED")
+  				while(c_message != /DISCONNECTED/)
   					c_message = client.gets
-  					c_message = command(c_message.strip, $1, client, @client_hash)
-  					client.puts c_message
+  					c_message = command(c_message.strip, username, client, @client_hash)
+  					if (c_message != nil)
+  						client.puts c_message
+  					end
   				end
-
-  				client.close
   			end
 		end
 
@@ -33,13 +33,16 @@ class Server
 		case recieved_m
 
 		when /BROADCAST\s+(.++)/
-			return_m = broadcast($1, client_hash)
+			return_m = broadcast($1, username, client_hash)
 		when /SEND\s+(\S+)\s+(.+)/
-			return_m = send(client_hash, $1, $2)
+			return_m = send(client_hash, username, $1, $2)
 		when /USERLIST/
 			return_m = userlist(client_hash, client)
 		when /DISCONNECT/
 			return_m = disconnect(client_hash, client, username)
+		when /RECIEVED/
+			return_m = nil
+			puts recieved_m
 		else
 			return_m = "Not a valid command"
 		end
@@ -54,11 +57,11 @@ class Server
 
 			c_message = client.gets
 			
-			if (/CONNECT\s+(\S+)/ =~ c_message)
-				puts $1
-				client_hash[$1] = client
-				client.puts "CONNECTED"
+			if (/CONNECT\s+(\S+)/ =~ c_message && client_hash.has_key?($1) != true) 
+ 				client_hash[$1] = client
+				client.puts "CONNECTED #{$1}"
 				connected = true
+				return $1
 			else
 				client.puts "FAILED"
 				connected = false
@@ -66,30 +69,29 @@ class Server
 		end
 	end
 
-	def broadcast(message, client_hash)
+	def broadcast(message, username, client_hash)
 		client_hash.each do |key, value|
-			value.puts message
+			value.puts "#{username} #{message}"
 		end
 		return "SENT"
 	end
 
-	def send(client_hash, user, message)
-		if (client_hash.has_key?(user))
-			client_hash[user].puts "SENTFROM #{username} #{message}"
+	def send(client_hash, username, user_to, message)
+		if (client_hash.has_key?(user_to))
+			client_hash[user_to].puts "SENTFROM #{username} #{message}"
 			return "SENT"
 		else 
-			return "#{user} is not a user."
+			return "FAILED #{user} is not a user."
 		end
 	end
 
 	def userlist(client_hash, client)
-		client.puts "USERS #{client_hash.keys.inspect}"
-		return "SENT"
+		return "USERS #{client_hash.keys.inspect}"
 	end
 
 	def disconnect(client_hash, client, username)
 		client_hash.delete(username)
-		return "DISCONNECTED"
+		return "DISCONNECTED #{username}"
 	end
 
 end
